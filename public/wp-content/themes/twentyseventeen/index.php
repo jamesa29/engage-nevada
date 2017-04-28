c339c53b8a753f1f83e9b6a5c3970fcf6aadf11f
james <james@echods.com>
2017-04-27 23:32:33 -0500

    fix container

--- public/wp-content/themes/twentyseventeen/index.php
+++ public/wp-content/themes/twentyseventeen/index.php
@@ -17,33 +17,36 @@
 
 get_header(); ?>
 
-	<div class="row">
-        
-        <div id="info" class="col-md-6">
+	<div class="container">
 
-            <div class="logo">
-            	<img src="<?php echo get_template_directory_uri() ?>/assets/img/EngageNevada-logo.png"/>
-            </div>
-            
-            <h2 class="text-center">VOTER REGISTRATION</h2>
+		<div class="row">
+	        
+	        <div id="info" class="col-md-6">
 
-            <p class="visible-xs">Voter registration takes an average of two minutes or less to complete</p>
-            
-            <div class="hidden-xs">
-            <hr>
-            <p>Voter registration takes an average of two minutes or less to complete.</p>
-            <p>Engage Nevada is a non-profit, non-partisan 501(c)(4) organization for civic engagement.</p>
-            <p>If you have a valid driver's license of identification card issued by the Nevada DMV, you can register online in Nevada. If you'd like to, select "I have a current NV state identification card or driver's license" and then click "Next Step" and we'll take you to the Nevada Secretary of State's online voter registration system.</p>
-            <p>If you don't have a state-issued ID, you can still register to vote using a paper form. Select "I don't have a current NV state identification card or driver's license" and then click "Next Step" and we'll take you to the Nevada Secretary of State website where you can print off your registration application.</p>
-            </div>
+		            <div class="logo">
+		            	<img src="<?php echo get_template_directory_uri() ?>/assets/img/EngageNevada-logo.png"/>
+		            </div>
+		            
+		            <h2 class="text-center">VOTER REGISTRATION</h2>
 
-        </div>
+		            <p class="visible-xs">Voter registration takes an average of two minutes or less to complete</p>
+		            
+		            <div class="hidden-xs">
+		            <hr>
+		            <p>Voter registration takes an average of two minutes or less to complete.</p>
+		            <p>Engage Nevada is a non-profit, non-partisan 501(c)(4) organization for civic engagement.</p>
+		            <p>If you have a valid driver's license of identification card issued by the Nevada DMV, you can register online in Nevada. If you'd like to, select "I have a current NV state identification card or driver's license" and then click "Next Step" and we'll take you to the Nevada Secretary of State's online voter registration system.</p>
+		            <p>If you don't have a state-issued ID, you can still register to vote using a paper form. Select "I don't have a current NV state identification card or driver's license" and then click "Next Step" and we'll take you to the Nevada Secretary of State website where you can print off your registration application.</p>
+		            </div>
 
-        <div id="form" class="col-md-6">
+		    </div>
+
+	        <div id="form" class="col-md-6">
             <?php echo do_shortcode('[gravityform id="2" title="false" description="true"]'); ?>
-        </div>
-    </div>
-
+    		</div>
+	    </div>
+	    
+	</div>
 
     <div class="row">
 	    <div class="col-md-12 text-center copyright hidden-xs">
